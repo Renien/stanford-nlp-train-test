aeb5d9c71519f261149db725a9393026c438c146
Renien <renien.john@gmail.com>
2016-07-09 06:43:49 +0530

    added the stanford project path

--- stanford_nlp_train_test/GenerateProps.py
+++ stanford_nlp_train_test/GenerateProps.py
@@ -1,8 +1,8 @@
 import fileinput
 import re
 
-class GenerateProps:
 
+class GenerateProps:
     def __init__(self, conf):
         self.conf = conf
         self.path = conf['trainFilePath']
@@ -10,13 +10,13 @@ class GenerateProps:
         self.file_range = conf['trainFileRange']
         self.file_list = []
 
-    def generate_files(self):
+    def _generate_files(self):
         for i in range(1, self.file_range):
-            self.file_list.append(self.path+'/'+self.file_name+str(i)+'.txt')
+            self.file_list.append(self.path + '/' + self.file_name + str(i) + '.txt')
         pass
 
     def update_props_file(self):
-        training_files = 'trainFile = '+';'.join(self.file_list)
-
-        for line in fileinput.input(self.conf['propsPath'], inplace = 1):
+        self._generate_files()  # Generate file list
+        training_files = 'trainFile = ' + ';'.join(self.file_list)
+        for line in fileinput.input(self.conf['projectPath']+'/'+self.conf['propsPath'], inplace=1):
             print re.sub('trainFile.*', training_files, line),
