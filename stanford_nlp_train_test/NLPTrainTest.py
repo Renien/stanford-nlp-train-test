36561eabe4dce8b92a2efceabfd58dabfacb4d31
Renien <renien.john@gmail.com>
2016-07-12 12:17:40 +0530

    Jar train/test execution command integrated with python

--- stanford_nlp_train_test/NLPTrainTest.py
+++ stanford_nlp_train_test/NLPTrainTest.py
@@ -7,3 +7,15 @@ class NLPTrainTest:
         Util.load_conf(conf_file)
         self.genProps = GenerateProps(Util.conf)
         self.genProps.update_props_file()
+
+    @staticmethod
+    def train_pos_tagger():
+        print Util.train + " Training started...... "
+        Util.execute_cmd(Util.train)
+        print "Training completed.."
+
+    @staticmethod
+    def test_pos_tagger():
+        print Util.train + " Testing started...... "
+        Util.execute_cmd(Util.test)
+        print "Testing completed.."
--- stanford_nlp_train_test/Util.py
+++ stanford_nlp_train_test/Util.py
@@ -3,6 +3,8 @@ import os
 
 # Hold the application conf
 conf = {}
+train = ""
+test = ""
 
 
 def load_conf(conf_file):
@@ -13,3 +15,30 @@ def load_conf(conf_file):
     global conf
     conf.clear()  # should keep the named reference to the mutable object
     conf.update(json.load(open(os.path.abspath(conf_file))))
+    _commands()
+
+
+def _commands():
+    os.chdir(conf["classPath"])
+    global train
+    train = "java -mx1g -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -props " + \
+            conf["propsPath"]
+    global test
+    test = "java -mx1g -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model " + \
+           conf["modelPath"] + " -testFile trainingData/corpus-1.txt -dump > " + conf["dumpFile"]
+
+
+# Execute the command in shell
+def execute_cmd(cmd):
+    import subprocess
+    std_out = subprocess.check_output([cmd], shell=True, stderr=subprocess.STDOUT)
+    _write_dump_file(std_out)  # Write the std out to the file
+    pass
+
+
+# Write the stdout to the file
+def _write_dump_file(std_out):
+    f = open(conf["dumpSTDOUT"], 'w')
+    f.write(std_out)
+    f.close()
+    pass
